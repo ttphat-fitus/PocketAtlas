94337f5139d584a4e2facae0e0fe20b6840220c0
Paul <ttphat2427@clc.fitus.edu.vn>
2025-12-11 15:36:30 +0700

    Update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  output: 'standalone',
   webpack: (config, { isServer }) => {
     return config;
   },
